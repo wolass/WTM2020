f2a16b8bff29ee7bc461e5cfbed8638339ac6d6f
Wojciech Francuzik <wolass@gmail.com>
2021-03-29 09:16:34 +0200

    typo - use RDS files

--- analysis/1_data_cleaning/data_cleaning_script.R
+++ analysis/1_data_cleaning/data_cleaning_script.R
@@ -12,6 +12,6 @@ pacman::p_load(tidyverse, here)
 
 # Add or remove variables
 
-# Export the data to here::here("analysis/data/derived_data/clean_df.Rmd")
+# Export the data to here::here("analysis/data/derived_data/clean_df.Rds")
 
-write_rds(final_df, here("analysis/data/derived_data/clean_df.Rmd"))
+write_rds(final_df, here("analysis/data/derived_data/clean_df.Rds")) # we want to use RDS files instead of csv. 
