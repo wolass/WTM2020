f4d5473edf777e89f83212008d1648c658594820
Krzysztof Rogozinski <csminespl@gmail.com>
2021-04-07 20:04:08 +0200

    #3 compare_fun - fixed for all patients

--- analysis/1_data_cleaning/data_cleaning_script.R
+++ analysis/1_data_cleaning/data_cleaning_script.R
@@ -51,16 +51,18 @@ names_json <- list.files(folder,pattern = "pacjent")
 names(json_data) <- names_json
 names(json_data) <- substr(names(json_data),8,9)
 names(json_data) <- as.numeric(names(json_data))
-names_json <- names(json_data)
 json_data <-  map(json_data,convert_fun)
 
-
+names(json_data)[6] <- 14.5
+names(json_data)[28] <- 34.5
+names_json <- names(json_data)
 #COMPARE
 compare_fun <- function(pacjent) {
   compare_list <- list()
   #pacjent <- as.character(pacjent)
 
   comp <- json_data[[pacjent]]
+  pacjent <- substr(pacjent,1,2)
   comp2 <-
     data %>% filter(record_id == pacjent,
                     redcap_event_name == "end_of_study_visit_arm_1") %>% select(
@@ -72,6 +74,7 @@ compare_fun <- function(pacjent) {
                       patient_temperature_6
                     )
   comp2 <- t(comp2) %>% as.data.frame()
+  comp2 <- comp2[!is.na(comp2)]
 
   comp3 <-
     data %>% filter(record_id == pacjent,
@@ -84,42 +87,41 @@ compare_fun <- function(pacjent) {
                       date_and_time_of_measure_6
                     )
   comp3 <- t(comp3)
-  #comp$time <- sub("Z", "", comp$time)
-  #comp$time <- sub("T", " ", comp$time)
-  lubridate::as_datetime(comp$time, tz = "Europe/Berlin") # Wykorzystujemy magię tidyverse do pracy z czasem!
-  # Tą paczkę musisz znać jak chcesz być data analyst :)
+  comp$time <-lubridate::as_datetime(comp$time, tz = "Europe/Berlin")
 
-  #comp <- as.data.frame(comp)
-  #comp3 <- ifelse(comp3 == "", NA, paste0(comp3, ":00"))
-  lubridate::ymd_hm(comp3[,1])
-  # tutaj rób dalej
+  comp <- as.data.frame(comp)
+  comp3 <-lubridate::ymd_hm(comp3[,1],tz = "Europe/Berlin")
+  comp3 <- comp3[!is.na(comp3)]
 
+  test_fun <- function(i){
+    w <- which(abs(comp3[i]-comp$time) == min(abs(comp3[i]-comp$time)))
+    w <- ceiling(sum(w)/length(w))
+    return(w) }
+  test <- map(1:length(comp3),test_fun)
+  test <- unlist(test)
+  comp <- comp[c(test),] %>% select(time,value)
+
+  comp3 <- cbind(comp3,comp$time)
   comp4 <- cbind(comp3, comp2)
-  colnames(comp4) <- c("time", "standard")
+  colnames(comp4) <- c("time","time2", "standard")
+  comp4 <- as.data.frame(comp4)
+  comp4$time <- lubridate::as_datetime(comp4$time,tz = "Europe/Berlin")
+  colnames(comp) <- c("time2","value")
+  #comp <- distinct(comp)
+  comp <- merge(comp, comp4, by = "time2",all.y=T) %>% distinct()
 
-  comp <- comp %>% filter(time %in% comp3) %>% select(time, value)
-  comp <- merge(comp, comp4, by = "time", all.x = T)
-  #comp <- cbind(comp2,comp)
-  #colnames(comp) <- c("standard","time","sensor")
-  #comp <- comp %>% filter(!is.na(standard))
   compare_list$comp <- comp
   compare_list$comp2 <- comp2
   compare_list$comp3 <- comp3
   compare_list$comp4 <- comp4
   return(compare_list)
 }
+test <- compare_fun("2")
 
 
-#zrobilem funckje, mam dwa problemy:
-#funckji nie da sie zaczytac purrem od razu na wszystkie, tylko recznie
-# WOLASS : Rozwiazanie "problemu nr 1"
-output <- map(names(json_data) %>% as.numeric, # zamiast obiektu na wejściu funcji podałeś liczbę.
-    compare_fun)
-# niestety funkcja compare_fun jest zle napisana więc wyrzuca error.
-
-# mam inne godziny lub brakuje tych godzin kiedy byl pomiar zwyklym termometrem
-# WOLASS : Odpowiedz napisałem w issue #6 i zupdateowałem kod powyżej. linijka 89.
-
+output <- map(names(json_data) #%>% as.numeric, # zamiast obiektu na wejściu funcji podałeś liczbę.
+    ,compare_fun)
+names(output) <- names_json
 
 # Export the data to here::here("analysis/data/derived_data/clean_df.Rds")
 
